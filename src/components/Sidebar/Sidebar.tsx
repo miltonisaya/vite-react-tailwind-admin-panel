000b92ceb8dcf2a48c49c5e9dd1438d339c72446
Milton Isaya <miltonisaiah@gmail.com>
2025-07-28 12:07:52 +0300

    added route select

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,14 @@
-import AccountToggle from "./accountToggle.tsx";
+import AccountToggle from "./AccountToggle.tsx";
+import Search from "./Search.tsx";
+import RouteSelect from "./RouteSelect.tsx";
 
 const Sidebar = () => {
   return (
     <div>
       <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
-        {/*    Main sidebar content*/}
         <AccountToggle />
+        <Search />
+        <RouteSelect />
       </div>
       {/*    Plan toggle*/}
     </div>
